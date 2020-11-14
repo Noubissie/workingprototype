b0d80ca5f48921a68b8be2663f6ce67166d340e2
NOUBISSIE <landryplacid@gmail.com>
2020-11-14 16:46:42 +0100

    new things

---
+++ precache-manifest.739e15e23471c1635709fc47ec81f169.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccb810a93934bca34175a0dd30eb634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "582fc4e24782ed272a8c",
+    "url": "/static/css/main.101ecae0.chunk.css"
+  },
+  {
+    "revision": "304d257ac41202d0ed41",
+    "url": "/static/js/2.ec31a933.chunk.js"
+  },
+  {
+    "revision": "dac7c182d18762d9f4372364245038b1",
+    "url": "/static/js/2.ec31a933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582fc4e24782ed272a8c",
+    "url": "/static/js/main.d428bac5.chunk.js"
+  },
+  {
+    "revision": "9b01bea236db5dd6fe52",
+    "url": "/static/js/runtime-main.59e73e23.js"
+  },
+  {
+    "revision": "23d1014dc6fd14f53f572d707cbd368c",
+    "url": "/static/media/falll.23d1014d.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/favicon.d41d8cd9.ico"
+  },
+  {
+    "revision": "5d6967cb5d6a33b8300a7500f3631bca",
+    "url": "/static/media/github.5d6967cb.svg"
+  },
+  {
+    "revision": "c7b235315647688376721efe5c84edc7",
+    "url": "/static/media/landingimage.c7b23531.svg"
+  },
+  {
+    "revision": "d3db73a5b309e004301cbbf2cc2ff693",
+    "url": "/static/media/map1.d3db73a5.svg"
+  },
+  {
+    "revision": "9995acc33bd9f95648d2680bc3287055",
+    "url": "/static/media/map2.9995acc3.svg"
+  },
+  {
+    "revision": "db496bfb41c01aa7101fa50e097bac6a",
+    "url": "/static/media/medical.db496bfb.svg"
+  },
+  {
+    "revision": "98ac656e7064c2add3efe171c528c3cb",
+    "url": "/static/media/moon.98ac656e.svg"
+  },
+  {
+    "revision": "b8113e40f02e823fbfdea889fb73c481",
+    "url": "/static/media/rlpBackground.b8113e40.svg"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/static/media/roboto-v18-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "376e0950b361fbd3b09508031f498de5",
+    "url": "/static/media/roboto-v18-latin-700.376e0950.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/static/media/roboto-v18-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "cae5027f600d2a0d88ac309655618e31",
+    "url": "/static/media/roboto-v18-latin-700.cae5027f.ttf"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/static/media/roboto-v18-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "372d0cc3288fe8e97df49742baefce90",
+    "url": "/static/media/roboto-v18-latin-regular.372d0cc3.ttf"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/static/media/roboto-v18-latin-regular.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "68889c246da2739681c1065d15a1ab0b",
+    "url": "/static/media/roboto-v18-latin-regular.68889c24.eot"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/static/media/roboto-v18-latin-regular.8681f434.svg"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/static/media/roboto-v18-latin-regular.bafb105b.woff"
+  },
+  {
+    "revision": "882e4d2178216cc2e8cd23827696e888",
+    "url": "/static/media/stackoverflow.882e4d21.svg"
+  },
+  {
+    "revision": "2a5a3b478475adb24f3f8c8093a6267a",
+    "url": "/static/media/sun.2a5a3b47.svg"
+  },
+  {
+    "revision": "d65716341ae94131ea2bbc281ced6422",
+    "url": "/static/media/telegram.d6571634.svg"
+  },
+  {
+    "revision": "c87b2aef17c88cc57501c9a12ac4a666",
+    "url": "/static/media/twitter.c87b2aef.svg"
+  }
+]);
